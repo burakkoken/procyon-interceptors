64b443d514f73d067e1ba6b4fa9a2f6a57ba4712
Burak Köken <burakkokenn@gmail.com>
2020-12-22 21:59:38 +0200

    Registered the User controller and Custom interceptor.

--- init.go
+++ init.go
@@ -1,5 +1,12 @@
 package main
 
-func init() {
+import (
+	"github.com/burakkoken/procyon-interceptors/controller"
+	"github.com/burakkoken/procyon-interceptors/interceptor"
+	core "github.com/procyon-projects/procyon-core"
+)
 
+func init() {
+	core.Register(controller.NewUserController)
+	core.Register(interceptor.NewCustomInterceptor)
 }
